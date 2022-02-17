9cf645869d39077ceda6a0d42a5b2d4e52730696
Michael Brown <mjbrown009@gmail.com>
2022-02-18 06:21:07 +1100

    Deleted const port replicate

--- app.js
+++ app.js
@@ -286,7 +286,6 @@ app.post('/deleteproduct', async (req, res) => {
   req.session.meter = 1;
   return res.redirect('back');
 });
-const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}!`);
 });
